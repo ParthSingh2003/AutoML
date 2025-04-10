f5780c88db4a9ecf214ea920666b9259f61f6005
ParthSingh2003 <parthsingh0829@gmail.com>
2025-04-10 22:42:53 +0530

    Fronted Trial 1

--- automl/src/App.jsx
+++ automl/src/App.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-// import DatasetUploader from "./components/pages/DatasetUploaderPage";
-
-
-// function App() {
-//   return (
-//     <div className="container mx-auto p-4">
-//       <h1 className="text-2xl font-bold mb-4">Upload Dataset</h1>
-//       <DatasetUploader />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartUpPage from "./components/pages/StartUpPage";
